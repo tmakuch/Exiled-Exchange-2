db4f238955fc9a1c5d2dd1b60db495436d939279
Tadeusz Makuch <tadeusz.makuch@gmail.com>
2025-01-04 18:50:19 +0100

    chore: safety checks to ignore empty or not full identifier, or empty filters all together

--- main/src/filter-generator/utils/parseRawFilters.ts
+++ main/src/filter-generator/utils/parseRawFilters.ts
@@ -8,10 +8,20 @@ const customFilterModifiers = {
 };
 
 export default function parseRawFilters(rawFilters: Array<IRawFilter>): Array<IFilter> {
-  return rawFilters.map((filter: IRawFilter) => ({
+  return rawFilters.filter((filter: IRawFilter) => {
+    const identifiersCount = filter.identifiers.filter((identifier: IRawFilter['identifiers'][number]) => {
+      return identifier.key.length > 0 && identifier.value.length > 0;
+    }).length;
+
+    //filter out empty raw filters (with no specified or not full identifiers)
+    return identifiersCount > 0;
+  }).map((filter: IRawFilter) => ({
       name: filter.name,
       identifiers: filter.identifiers.reduce((result, next: {key: string, value: string }) => {
-        result[next.key] = next.value.includes(',') ? next.value.split(",") : next.value;
+        //filter empty or not full identifiers
+        if (next.key.length > 0 && next.value.length > 0) {
+          result[next.key] = next.value.includes(',') ? next.value.split(",") : next.value;
+        }
         return result;
       }, {} as Record<string, string | Array<string>>),
       hide: filter.hide,
